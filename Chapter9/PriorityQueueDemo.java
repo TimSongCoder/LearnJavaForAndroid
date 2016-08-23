5fc49ddf60ced09fbadfefa721e61e591e156079
TimSong <timmy_song@163.com>
2016-08-23 15:10:59 +0800

    Modify output order.

--- Chapter9/PriorityQueueDemo.java
+++ Chapter9/PriorityQueueDemo.java
@@ -1,9 +1,14 @@
 import java.util.Queue;
 import java.util.PriorityQueue;
+import java.util.Comparator;
 
 public class PriorityQueueDemo{
 	public static void main(String[] args){
-		Queue<Integer> qi = new PriorityQueue<Integer>();
+		Queue<Integer> qi = new PriorityQueue<Integer>(new Comparator<Integer>(){
+			public int compare(Integer e1, Integer e2){
+				return e2 - e1; //  for descending order.
+			}
+		});
 		for(int i=0; i<15; i++){
 			qi.add((int)(Math.random()*100));  // Randomly generated a integer range from 0 to 99 inclusively.
 		}
