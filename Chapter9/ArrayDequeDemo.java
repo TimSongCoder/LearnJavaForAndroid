c80164645139d9be462344797be0ff17bdbd415c
TimSong <timmy_song@163.com>
2016-08-24 14:17:39 +0800

    Add comments to understand.

--- Chapter9/ArrayDequeDemo.java
+++ Chapter9/ArrayDequeDemo.java
@@ -4,9 +4,9 @@ import java.util.Deque;
 public class ArrayDequeDemo{
 	public static void main(String[] args){
 		Deque<String> stackDeque = new ArrayDeque<String>();
-		String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // Will use the deque as stack.
 		for(String weekday: weekdays){
-			stackDeque.push(weekday);  // insert
+			stackDeque.push(weekday);  // insert  first in, last out "stack-like behavior" through the push\pop method.
 		}
 		
 		while(stackDeque.peek()!=null){ // examine
