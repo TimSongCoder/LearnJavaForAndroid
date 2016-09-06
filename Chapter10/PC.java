9b084a311acf033b17f31f12719115a821446b34
TimSong <timmy_song@163.com>
2016-09-06 11:38:51 +0800

    Synchronization in terms of Lock and Condition.

--- Chapter10/PC.java
+++ Chapter10/PC.java
@@ -2,19 +2,29 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PC{
 	public static void main(String[] args){
+		final Lock lock = new ReentrantLock();
 		final BlockingQueue<Character> bq = new ArrayBlockingQueue<Character>(26);
 		final ExecutorService executor = Executors.newFixedThreadPool(2);
 		Runnable producer = new Runnable(){
 			public void run(){
 				for(char ch='A';ch <= 'Z';ch++){
+					lock.lock();
 					try{
-						bq.put(ch);
+						while(!bq.offer(ch)){ // full and not insert
+							lock.unlock();
+							Thread.sleep(50);
+							lock.lock();  // reacquire the lock, or reentrant
+						}
 						System.out.println(ch + " produced by " + Thread.currentThread());
 					}catch(InterruptedException ie){
 						ie.printStackTrace();
+					}finally{
+						lock.unlock();
 					}
 				}
 			}
@@ -26,10 +36,19 @@ public class PC{
 				char ch = '\0';
 				do{
 					try{
-						ch = bq.take();
+						lock.lock();
+						Character c;
+						while((c = bq.poll()) == null){ // empty, no elements can be polled.
+							lock.unlock();
+							Thread.sleep(50);
+							lock.lock();  // reentrant
+						}
+						ch = c.charValue();
 						System.out.println(ch + " consumed by " + Thread.currentThread());
 					}catch(InterruptedException ie){
 						ie.printStackTrace();
+					}finally{
+						lock.unlock();
 					}
 				}while(ch!='Z');
 				executor.shutdownNow();
---
+++ Chapter10/PC2.java
@@ -0,0 +1,159 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PC2{
+	public static void main(String[] args){
+		Shared s = new Shared();
+		
+		new Consumer(s).start();
+		
+		// Simulate the delay and verify the Lock, Condition mechanics.
+		try{
+			Thread.sleep(1000);
+		}catch(InterruptedException ie){
+			ie.printStackTrace();
+		}
+		
+		new Producer(s).start();
+		
+		System.out.println(Thread.currentThread() + " is ending...");
+	}
+}
+
+class Shared{
+	/* Fields c and available are volatile so that writes to them are visible to the various threads.
+	* Fields lock and condition are final so that there initial values are visible to the various threads.
+	* The Java memory model promises that, after a final field has been initialized, any thread will see the same
+	* correct value.
+	*/
+	private volatile char c;
+	private volatile boolean available;
+	private final Lock lock;
+	private final Condition condition;
+	
+	Shared(){
+		c = '\u0000';
+		available = false;
+		lock = new ReentrantLock();
+		condition = lock.newCondition(); // bound
+		System.out.println("Condition initialized: " + condition);
+	}
+	
+	Lock getLock(){
+		return lock;
+	}
+	
+	char getSharedChar(){
+		System.out.println("getSharedChar: " + lock);
+		lock.lock();
+		try{
+			while(!available){
+				try{
+					condition.await();  // release the lock and wait.
+				}catch(InterruptedException ie){
+					ie.printStackTrace();
+				}	
+			}
+			available = false;
+			condition.signal();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			lock.unlock();
+			return c;  // where volatile is neccesary, the field is modified and accessed from multiple threads. It needs visibility.
+		}
+	}
+	
+	void setSharedChar(char c){
+		System.out.println("setSharedChar: " + lock);
+		lock.lock();
+		try{
+			while(available){
+				try{
+					condition.await();  // field c is available for getting. wait and release the lock.
+				}catch(InterruptedException ie){
+					ie.printStackTrace();
+				}
+			}
+			this.c = c;
+			available = true;
+			condition.signal();
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			lock.unlock();
+			System.out.println("After setSharedChar unlock: " + lock); 
+			// The lock is still locked by ProducerThread because of holder count not be zero.
+		}
+	}
+}
+
+class Producer extends Thread{
+	// field lock and shared are final because it's initialized on the main thread and accessed on the other thread.
+	private final Lock lock;
+	
+	private final Shared shared;
+	
+	Producer(Shared s){
+		shared = s;
+		lock = s.getLock();
+		setName("ProducerThread");
+	}
+	
+	@Override
+	public void run(){
+		System.out.println("Producer is starting...");
+		System.out.println("Producer: " + lock);
+		
+		for(char ch = 'A'; ch<='Z';ch++){
+			lock.lock(); // repeatedly called.
+			shared.setSharedChar(ch);
+			try{
+				Thread.sleep(100);
+			}catch(InterruptedException ie){
+				ie.printStackTrace();
+			}
+			System.out.println("Producer afterSetChar: " + lock);
+			
+			System.out.println(ch + " produced by producer.");
+			lock.unlock();
+		}
+		
+		System.out.println("Producer is ending...");
+	}
+}
+
+class Consumer extends Thread{
+	private final Lock lock;
+	
+	private final Shared shared;
+	
+	Consumer(Shared s){
+		shared = s;
+		lock = s.getLock();
+		setName("ConsumerThread");
+	}
+	
+	@Override
+	public void run(){
+		System.out.println("Consumer is starting...");
+		System.out.println("Consumer: " + lock);
+		char ch;
+		do{
+			lock.lock();
+			ch = shared.getSharedChar();
+			try{
+				Thread.sleep(100);
+			}catch(InterruptedException ie){
+				ie.printStackTrace();
+			}
+			System.out.println("Consumer afterGetChar: " + lock);
+			
+			System.out.println(ch + " consumed by consumer.");
+			lock.unlock();
+		}while(ch!='Z');	
+		
+		System.out.println("Consumer is ending...");
+	}
+}
