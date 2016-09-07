21d38b0978079e35dcb96b41dd15bf1ca319f58d
TimSong <timmy_song@163.com>
2016-09-07 11:16:49 +0800

    Specify pool-thread name through ThreadFactory interface.

--- Chapter10/CountingApp.java
+++ Chapter10/CountingApp.java
@@ -1,9 +1,16 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class CountingApp{
 	public static void main(String[] args){
-		ExecutorService es = Executors.newFixedThreadPool(2);
+		ExecutorService es = Executors.newFixedThreadPool(2, new ThreadFactory(){
+			char threadName = 'A';
+			public Thread newThread(Runnable r){
+				// with limitations: pool capacity is less than 26.
+				return new Thread(r, String.valueOf(threadName++));
+			}
+		});
 		Runnable countRunnable = new Runnable(){
 			public void run(){
 				int count = 0;
