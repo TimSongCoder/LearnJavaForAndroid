4651fb18b54e6ed5d3c5d11efe2e36a4ca87b49e
TimSong <timmy_song@163.com>
2016-09-24 12:10:14 +0800

    Serialization for object-oriented IO operation.

---
+++ Chapter11/SerializationDemo.java
@@ -0,0 +1,47 @@
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SerializationDemo{
+	private static final String FILE_PATH = "employee.dat";
+	
+	public static void main(String[] args){
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+		try{
+			FileOutputStream fos = new FileOutputStream(FILE_PATH);
+			oos = new ObjectOutputStream(fos);
+			Employee emp = new Employee("John Doe", 36);
+			oos.writeObject(emp);
+			oos.close();
+			oos = null;
+			
+			FileInputStream fis = new FileInputStream(FILE_PATH);
+			ois = new ObjectInputStream(fis);
+			emp = (Employee)ois.readObject();
+			System.out.println("Deserialization: " + emp.getName());
+			System.out.println("Deserialization: " + emp.getAge());
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}catch(ClassNotFoundException cnfe){
+			cnfe.printStackTrace();
+		}finally{
+			if(oos != null){
+				try{
+					oos.close();
+				}catch(IOException ioe){
+					ioe.printStackTrace();
+				}
+			}
+			if(ois !=null){
+				try{
+					ois.close();
+				}catch(IOException ioe){
+					ioe.printStackTrace();
+				}
+			}
+		}
+	}
+}
