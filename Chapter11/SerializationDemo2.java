04169075d46f0f27220f118698f8432081c29736
TimSong <timmy_song@163.com>
2016-09-26 14:16:20 +0800

    Deserialisation failed because of super class having no valid constructor for deserialization.

---
+++ Chapter11/SerializationDemo2.java
@@ -0,0 +1,68 @@
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
+// Non-serializable Super class
+class Employee{
+	private String name;
+	
+	Employee(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+}
+
+// A walkaround to solve the non-serializable super class
+class SerEmployee extends Employee implements Serializable{
+	SerEmployee(String name){
+		super(name);
+	}
+}
+
+public class SerializationDemo2{
+	public static void main(String[] args){
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+		try{
+			FileOutputStream fos = new FileOutputStream("employees.dat");
+			oos = new ObjectOutputStream(fos);
+			SerEmployee se = new SerEmployee("John Doe");
+			oos.writeObject(se);
+			System.out.println("se object writeten to file.");
+			oos.close();
+			oos = null;
+			
+			FileInputStream fis = new FileInputStream("employees.dat");
+			ois = new ObjectInputStream(fis);
+			se = (SerEmployee)ois.readObject();
+			System.out.println("se object read from file.");
+			System.out.println(se);
+		}catch(ClassNotFoundException cnfe){
+			cnfe.printStackTrace();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}finally{
+			if(oos!=null){
+				try{
+				oos.close();
+				}catch(IOException ioe){
+					ioe.printStackTrace();
+				}
+			}
+			if(ois!=null){
+				try{
+					ois.close();
+				}catch(IOException ioe){
+					ioe.printStackTrace();
+				}
+			}
+		}
+	}
+}
