f3e078d55651d3f3bcf1a19a58c7706a64bf6ca9
TimSong <timmy_song@163.com>
2016-09-27 16:04:28 +0800

    Verify the line separator value on Windows platform.

--- Chapter7/System/SystemDemo.java
+++ Chapter7/System/SystemDemo.java
@@ -4,5 +4,11 @@ public class SystemDemo{
 		for(int i=0; i< propertyNames.length; i++){
 			System.out.println(propertyNames[i] + " : " + System.getProperty(propertyNames[i]));
 		}
+		
+		String lineSeparator = System.getProperty("line.separator");
+		for(int i=0;i<lineSeparator.length();i++){
+			char separatorChar = lineSeparator.charAt(i);
+			System.out.println("line.separator: " + (int)separatorChar);
+		}
 	}
 }
