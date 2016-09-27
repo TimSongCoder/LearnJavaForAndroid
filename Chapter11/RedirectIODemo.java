c4aa1ed699e36a549055a130cc4f8197c552bbdf
TimSong <timmy_song@163.com>
2016-09-27 16:05:07 +0800

    Redirect Standard IO through System class setIn setOut setErr methods.

---
+++ Chapter11/RedirectIODemo.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.io.IOException;
+
+public class RedirectIODemo{
+	public static void main(String[] args) throws IOException{
+		if(args.length!=3){
+			System.err.println("usage: java RedirectIODemo inputSource outDest errDest.");
+			return;
+		}
+		System.setIn(new FileInputStream(args[0]));
+		System.setOut(new PrintStream(args[1], "gbk"));
+		System.setErr(new PrintStream(args[2]));
+		
+		int ch;
+		while((ch=System.in.read())!=-1){
+			System.out.print((char)ch);
+		}
+		
+		System.err.println("This is error log.");
+		System.err.println();
+		System.err.println("Permission Denied." + new Date(System.currentTimeMillis()));
+	}
+}
