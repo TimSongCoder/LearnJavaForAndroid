f8f2e434826b60624bff12f50ffe808b2a2fd546
TimSong <timmy_song@163.com>
2016-09-30 09:22:11 +0800

    Limit the significant number or precision to 3.

--- Chapter11/CircleInfo.java
+++ Chapter11/CircleInfo.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class CircleInfo{
 	public static void main(String[] args){
@@ -10,8 +12,8 @@ public class CircleInfo{
 				System.out.println("Enter the radius of the circle:");
 				String text = br.readLine();
 				double radius = Double.parseDouble(text);
-				System.out.println("Circumference: " + 2*Math.PI*radius);
-				System.out.println("Area: " + Math.PI*radius*radius);
+				System.out.println("Circumference: " + getRoundedResult(2*Math.PI*radius));
+				System.out.println("Area: " + getRoundedResult(Math.PI*radius*radius));
 				System.out.println();
 			}while(true);
 		}catch(NumberFormatException nfe){
@@ -19,5 +21,11 @@ public class CircleInfo{
 		}catch(IOException ioe){
 			ioe.printStackTrace();
 		}
+	}
+	
+	static double getRoundedResult(double original){
+		BigDecimal bd = new BigDecimal(original);
+		bd = bd.round(new MathContext(3));
+		return bd.doubleValue();
 	}
 }
