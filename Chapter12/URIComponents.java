3e83115a0849eae0f3f7a62635122769b0b1b973
TimSong <timmy_song@163.com>
2016-10-10 11:40:48 +0800

    Explored the URI APIs simply.

---
+++ Chapter12/URIComponents.java
@@ -0,0 +1,29 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class URIComponents{
+	
+	public static void main(String[] args){
+		if(args.length != 1){
+			System.err.println("usage: java URIComponents uri");
+			return;
+		}
+		try{
+			URI uri = new URI(args[0]);
+			
+			System.out.println("Authority = " + uri.getAuthority());
+			System.out.println("Fragment = " + uri.getFragment());
+			System.out.println("Host = " + uri.getHost());
+			System.out.println("Path = " + uri.getPath());
+			System.out.println("Port = " + uri.getPort());
+			System.out.println("Query = " + uri.getQuery());
+			System.out.println("Scheme = " + uri.getScheme());
+			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+			System.out.println("User info = " + uri.getUserInfo());
+			System.out.println("URI is absolute: " + uri.isAbsolute());
+			System.out.println("URI is opaque: " + uri.isOpaque());
+		}catch(URISyntaxException use){
+			use.printStackTrace();
+		}
+	}
+}
