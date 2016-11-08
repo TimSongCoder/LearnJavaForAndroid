5efd602be81fca904611dbf3103c358d81a4a2ce
TimSong <timmy_song@163.com>
2016-11-08 16:25:21 +0800

    Add import statements, all things worked out finally.

--- Chapter15/DOMDemo.java
+++ Chapter15/DOMDemo.java
@@ -1,3 +1,19 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
 public class DOMDemo{
 
 	public static void main(String[] args){
