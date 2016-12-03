8e749aa63d0e8707d1896b32d3cf79dd428e85e7
HP_TIM <timmy_song@163.com>
2016-12-03 12:30:31 +0800

    Refactor the class to fetch resource from jar file.

--- Chapter16/ResourceLoadDemo.java
+++ Chapter16/ResourceLoadDemo.java
@@ -13,13 +13,38 @@ public class ResourceLoadDemo{
       return ;
     }
     try{
-      byte[] image = new byte[(int)new File(RESOURCE_FILE).length()];
-      int rd, i = 0;
-      while((rd=is.read())!=-1){
-        image[i++] = (byte)rd;
-      }
-      for(i=0;i<16;i++){
-        System.out.printf("%02X ", image[i]);
+      byte[] unit = new byte[1024];
+      int rd, length = 0;
+      if(is.markSupported()){
+        System.out.println("markSupported is true.");
+        is.mark(Integer.MAX_VALUE);
+        while((rd=is.read())!=-1){
+          length+=rd;
+        }
+        byte[] image = new byte[length];
+        int i=0;
+        is.reset();
+        while((rd=is.read())!=-1){
+          image[i++] = (byte)rd;
+        }
+        for(i=0;i<16;i++){
+          System.out.printf("%02X ", image[i]);
+        }
+      }else{
+        System.out.println("markSupported is false, need alternative approach:)");
+        while((rd=is.read())!=-1){
+          length+=rd;
+        }
+        byte[] image = new byte[length];
+        // Obtain a new InputStream to read.
+        InputStream nis = ResourceLoadDemo.class.getResourceAsStream(RESOURCE_FILE);
+        int i =0;
+        while((rd=nis.read())!=-1){
+          image[i++] = (byte)rd;
+        }
+        for(i=0;i<16;i++){
+          System.out.printf("%02X ", image[i]);
+        }
       }
     }catch(IOException ioe){
       ioe.printStackTrace();
