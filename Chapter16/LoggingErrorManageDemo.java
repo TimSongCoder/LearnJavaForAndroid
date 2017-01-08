e10a2493d10fd41243faab1a6b929915605b237e
HP_TIM <timmy_song@163.com>
2017-01-08 12:30:14 +0800

    Verify the ErrorManager behavior deeper.

--- Chapter16/LoggingErrorManageDemo.java
+++ Chapter16/LoggingErrorManageDemo.java
@@ -5,13 +5,16 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.util.logging.StreamHandler;
 import java.util.logging.SimpleFormatter;
+import java.util.logging.FileHandler;
 
 public class LoggingErrorManageDemo{
   public static void main(String[] args) throws IOException{
     Logger logger = Logger.getLogger("LoggingErrorManageDemo");
     FileOutputStream fos = new FileOutputStream("log");
     StreamHandler sh = new StreamHandler(fos, new SimpleFormatter());
+    FileHandler fh = new FileHandler("logFile");
     logger.addHandler(sh);
+    logger.addHandler(fh);
     for (int i=0; i<5; i++) {
       logger.log(Level.WARNING, String.valueOf(i));
       if(i==2){
@@ -21,5 +24,7 @@ public class LoggingErrorManageDemo{
       }
     }
     System.out.println("---------Logging error has been handled by ErrorManager associated with Handler, so we can move on.---------");
+
+    logger.log(Level.INFO, "CAN NOT UNISTALL PRE-INSTALLED APP ON SAMSUNG.\n");
   }
 }
