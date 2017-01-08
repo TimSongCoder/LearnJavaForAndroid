af7a3bea615e45247f06523eba2b66abd29b7c31
HP_TIM <timmy_song@163.com>
2017-01-08 12:17:27 +0800

    Logging framework handles the errors emerged during the logging process through ErrorManager associated with Handler, such that it does not affect the application execution in case of its internal error.

---
+++ Chapter16/LoggingErrorManageDemo.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.FileOutputStream;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.StreamHandler;
+import java.util.logging.SimpleFormatter;
+
+public class LoggingErrorManageDemo{
+  public static void main(String[] args) throws IOException{
+    Logger logger = Logger.getLogger("LoggingErrorManageDemo");
+    FileOutputStream fos = new FileOutputStream("log");
+    StreamHandler sh = new StreamHandler(fos, new SimpleFormatter());
+    logger.addHandler(sh);
+    for (int i=0; i<5; i++) {
+      logger.log(Level.WARNING, String.valueOf(i));
+      if(i==2){
+        fos.close();
+        // The logging framework, the logging Handler specifically will meet an error
+        // after the FileOutputStream has been closed prematurely.
+      }
+    }
+    System.out.println("---------Logging error has been handled by ErrorManager associated with Handler, so we can move on.---------");
+  }
+}
